3e30a276685fc32cc89fcb9d30723c99cf2a9c3a
zdzislawkoperek <zdzislawkoperek@gmail.com>
2019-12-11 16:28:12 +0100

    cleened

--- first.php
+++ first.php
@@ -3,12 +3,6 @@
 <title>title1</title>
 </head>
 <body>
-is this in branch also?
-title1 body <br>
-after first change <br>
-second change
-  third change on remote
-first change after second file
-is this a new branch?
+master branch
 </body>
 </html>
