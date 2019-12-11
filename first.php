785a60cf9253e1731efb44d79622d81d3d0f7e92
zdzislawkoperek <zdzislawkoperek@gmail.com>
2019-12-11 16:29:46 +0100

    cleen branch

--- first.php
+++ first.php
@@ -3,12 +3,7 @@
 <title>title1</title>
 </head>
 <body>
-title1 body <br>
-after first change <br>
-second change
-  third change on remote
-first change after second file
-is this a new branch?
+master branch
 kurwabranch
 </body>
 </html>
